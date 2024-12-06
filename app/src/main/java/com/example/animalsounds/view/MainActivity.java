3b939bb481ca29c0436c398c1d64e6967c4fbac8
isaurus <isaacmartin.dev@gmail.com>
2024-12-07 00:20:45 +0100

    Initial Commit

--- app/src/main/java/com/example/animalsounds/view/MainActivity.java
+++ app/src/main/java/com/example/animalsounds/view/MainActivity.java
@@ -1,12 +1,21 @@
-package com.example.animalsounds;
+package com.example.animalsounds.view;
 
+import android.content.Context;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.animalsounds.R;
+import com.example.animalsounds.controller.ImageController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,5 +53,27 @@ public class MainActivity extends AppCompatActivity {
                 finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void initComponents(){
+        List<ImageView> animalPicsList = new ArrayList<>();
+        animalPicsList.add(findViewById(R.id.imgPig));
+        animalPicsList.add(findViewById(R.id.imgRabbit));
+        animalPicsList.add(findViewById(R.id.imgChicken));
+        animalPicsList.add(findViewById(R.id.imgFox));
+        animalPicsList.add(findViewById(R.id.imgCow));
+        animalPicsList.add(findViewById(R.id.imgHorse));
+
+        List<SoundPool> animalSoundsList = new ArrayList<>();
+        animalSoundsList.add(R.raw.pig);
+        animalSoundsList.add(R.raw.rabbit);
+        animalSoundsList.add(R.raw.chicken);
+        animalSoundsList.add(R.raw.fox);
+        animalSoundsList.add(R.raw.cox);
+        animalSoundsList.add(R.raw.horse);
+
+        //Context ctx = getApplicationContext();
+        ImageController imgController = new ImageController();
+        imgController.bindListToXML(animalPicsList);
     }
 }
